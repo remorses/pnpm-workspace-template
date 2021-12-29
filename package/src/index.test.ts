0a89ecc23d4eae2595498abbab79875fb6cc4dc1
remorses <beats.by.morse@gmail.com>
2021-12-29 12:16:13 +0100

    added vitest test runner

--- package/src/index.test.ts
+++ package/src/index.test.ts
@@ -1,3 +1,14 @@
+import { test, expect } from 'vitest'
+
 test('ready', () => {
     expect(true).toBe(true)
+    expect({ x: 3 }).toMatchSnapshot()
 })
+
+test('two', () => {
+    expect(true).toBe(true)
+    console.log('hello')
+    expect({ x: 3 }).toMatchSnapshot()
+})
+
+
