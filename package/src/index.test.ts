00c6eb0c8671f47c04f6c59b78b06597ad434dbf
remorses <beats.by.morse@gmail.com>
2022-05-13 15:47:55 +0200

    npm:

--- package/src/index.test.ts
+++ package/src/index.test.ts
@@ -2,13 +2,8 @@ import { test, expect } from 'vitest'
 
 test('ready', () => {
     expect(true).toBe(true)
-    expect({ x: 3 }).toMatchSnapshot()
 })
 
 test('two', () => {
     expect(true).toBe(true)
-    console.log('hello')
-    expect({ x: 3 }).toMatchSnapshot()
 })
-
-
